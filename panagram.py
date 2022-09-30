6652f764f2df30d9a9d2ba7abe9953bc77c68b42
Brook Azene <bta5090@psu.edu>
2022-09-29 20:38:48 -0400

    Create panagram.py

---
+++ panagram.py
@@ -0,0 +1,1 @@
+# Not implemented: For a given array of strings, where each string as a element can be a sentence, phrase or combination of letters, is a panagram if it has every letter in the alphabet without repetition. If for every panagram in the array, a 1 is assigned, and 0 for any other strings. Therefore in an array of [panagram, random, panagram], a string will be returned as '101'.
